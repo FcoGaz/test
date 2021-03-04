5c049e2a39c17d9c8a7d155146708efe987a770e
juang <juang@DESKTOP-GDPNSVA>
2021-03-04 11:35:47 -0600

    VERSION 1.1

--- src/Test/HolaMundo.java
+++ src/Test/HolaMundo.java
@@ -3,8 +3,14 @@ package Test;
 public class HolaMundo {
 
     public static void main(String[] args) {
-
-        System.out.println("Hola Java 8 !!!");
+        
+        System.out.println("bienvinos a la clase POO");
+        // metacomandos:
+        // sout
+        // psvm
+        
     }
 
+    
+   
 }
